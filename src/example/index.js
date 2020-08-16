abbcbe0a2447266dd9eb3326e06d84fd1f0f009e
svill <villella.sam@gmail.com>
2020-08-17 01:08:17 +1000

    Added new event to emit multiple responses

--- src/example/index.js
+++ src/example/index.js
@@ -4,20 +4,38 @@ var btOBDReader = new OBDReader();
 var dataReceivedMarker = {};
 
 btOBDReader.on('connected', function () {
-    this.addPoller("temp");
-    this.addPoller("catemp11");
-    this.startPolling(1000); //Request all values each second.
+    sleep(3000).then(() => {
+        console.log('My Setup')
+        this.write('ATH1');
+        this.write("ATE1")
+        //this.addPoller("temp");
+        //this.addPoller("catemp11");
+        this.addPollerString("2101")
+        this.addPollerString("2102")
+        this.addPollerString("21E4")
+        this.addPollerString("21E0")
+
+        this.startPolling(1000); //Request all values each second.
+    });
 });
 
-btOBDReader.on('dataReceived', function (data) {
-    if (Object.keys(data).length !== 0) {
-      console.log(data);
-    }
-    dataReceivedMarker = data;
-});
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
 
-btOBDReader.on('messageReceived', function (data) {
-    console.log("MSG: " + data);
+// btOBDReader.on('dataReceived', function (data) {
+//     if (Object.keys(data).length !== 0) {
+//       console.log(data);
+//     }
+//     dataReceivedMarker = data;
+// });
+
+// btOBDReader.on('messageReceived', function (data) {
+//     console.log("MSG: " + data);
+// });
+
+btOBDReader.on('messagesReceived', function (data) {
+    console.log("Msgs (" + data.length + "): " + "***" + data + "***");
 });
 
 console.log('Connecting...')
